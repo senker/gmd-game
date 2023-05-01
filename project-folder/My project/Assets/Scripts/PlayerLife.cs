095c5fcbff74ba9e619ce0d2130cdf44352f8f9c
senker <senkerplov@gmail.com>
2023-05-01 15:51:27 +0200

    ❇️ feat: traps & death

---
+++ project-folder/My project/Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    private Animator _anim;
+    private PlayerMovement _pm;
+    
+    private static readonly int Death = Animator.StringToHash("Death");
+
+    void Start()
+    {
+        _pm = GetComponent<PlayerMovement>();
+        _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        _pm.enabled = false;
+        _rb.bodyType = RigidbodyType2D.Static;
+        _anim.SetTrigger(Death);
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
