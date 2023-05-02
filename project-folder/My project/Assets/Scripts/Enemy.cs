d71060687ab2299e4cdbb81f819245b4fffb5223
senker <senkerplov@gmail.com>
2023-05-02 17:27:36 +0200

    ❇️ feat: enemy always faces player, decorations

--- project-folder/My project/Assets/Scripts/Enemy.cs
+++ project-folder/My project/Assets/Scripts/Enemy.cs
@@ -1,19 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public GameObject player;
     public Animator animator;
     private Rigidbody2D _rb;
     private Collider2D _col;
     
     public int maxHealth = 100;
     int _currentHealth;
+    public bool flip;
+    public float speed;
 
     private static readonly int IsDead = Animator.StringToHash("IsDead");
     private static readonly int Hurt = Animator.StringToHash("Hurt");
 
+    private void Update()
+    {
+        Vector3 scale = transform.localScale;
+
+        if (player.transform.position.x > transform.position.x)
+        {
+            scale.x = Mathf.Abs(scale.x) * -1 * (flip ? -1 : 1);
+            //transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            scale.x = Mathf.Abs(scale.x) * (flip ? -1 : 1);
+            //transform.Translate(speed * Time.deltaTime * -1, 0, 0);
+        }
+        
+        transform.localScale = scale;
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
