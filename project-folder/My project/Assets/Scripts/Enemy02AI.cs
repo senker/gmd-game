31e0a663ff2d0338285941b8bf92982adba1b975
senker <senkerplov@gmail.com>
2023-05-07 18:52:39 +0200

    ❇️feat: enemy 02 take hit & death anim

--- project-folder/My project/Assets/Scripts/Enemy02AI.cs
+++ project-folder/My project/Assets/Scripts/Enemy02AI.cs
@@ -7,25 +7,36 @@ using UnityEngine.Serialization;
 
 public class Enemy02AI : MonoBehaviour
 {
+    public GameObject player;
     public Transform target;
+    private Seeker _seeker;
+    private Rigidbody2D _rb;
     [FormerlySerializedAs("enemyGFX")] public Transform enemyGfx;
-    
+    public Animator animator;
+    private Collider2D _col;
+
+
+    [SerializeField] private float knockbackForce = 10f;
     public float speed = 200f;
     public float nextWaypointDistance = 3f;
 
+    public int maxHealth = 100;
+    int _currentHealth;
     private Path _path;
     private int _currentWaypoint = 0;
     [SerializeField] private bool _reachedEndOfPath = false;
 
-    private Seeker _seeker;
-    private Rigidbody2D _rb;
+    
+    private static readonly int IsDead = Animator.StringToHash("IsDead");
+    private static readonly int Hurt = Animator.StringToHash("Hurt");
     
     // Start is called before the first frame update
     void Start()
     {
         _seeker = GetComponent<Seeker>();
         _rb = GetComponent<Rigidbody2D>();
-        
+        _col = GetComponent<CircleCollider2D>();
+        _currentHealth = maxHealth;
         InvokeRepeating(nameof(UpdatePath), 0f, .5f);
     }
 
@@ -45,10 +56,6 @@ public class Enemy02AI : MonoBehaviour
         }
     }
 
-    private void Update()
-    {
-
-    }
 
     void FixedUpdate()
     {
@@ -59,8 +66,7 @@ public class Enemy02AI : MonoBehaviour
         {
             _reachedEndOfPath = true;
             return;
-        }
-        else
+        } else
         {
             _reachedEndOfPath = false;
         }
@@ -85,5 +91,38 @@ public class Enemy02AI : MonoBehaviour
         {
             enemyGfx.localScale = new Vector3(1f, 1f, 1f);
         }
+    }
+    
+    public void TakeDamage(int damage)
+    {
+        _currentHealth -= damage;
+        animator.SetTrigger(Hurt);
+        
+        
+        if (_currentHealth <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            // Calculate the knockback direction based on the direction from the player to the enemy
+            Vector2 knockbackDirection = transform.position - player.transform.position;
+
+            // Normalize the direction to get a unit vector
+            knockbackDirection.Normalize();
+
+            // Apply the knockback force in the calculated direction
+            _rb.AddForce(knockbackDirection * knockbackForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void Die()
+    {
+        // Die anim
+        animator.SetBool(IsDead, true);
+        // Disable the enemy
+        _rb.isKinematic = true;
+        enabled = false;
+        _col.enabled = false;
     }
 }
--- project-folder/My project/Assets/Scripts/PlayerCombat.cs
+++ project-folder/My project/Assets/Scripts/PlayerCombat.cs
@@ -93,6 +93,7 @@ public class PlayerCombat : MonoBehaviour
         foreach (Collider2D enemy in hitEnemies)
         {
             enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+            enemy.GetComponent<Enemy02AI>().TakeDamage(attackDamage);
         }
     }
 
